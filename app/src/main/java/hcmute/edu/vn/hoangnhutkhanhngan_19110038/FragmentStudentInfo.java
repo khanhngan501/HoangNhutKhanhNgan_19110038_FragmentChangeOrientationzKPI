c004c4ce7bd71c07cdf61a028fc7bc368c4f4c84
khanhngan501 <khanhngan501@gmail.com>
2022-05-08 23:41:26 +0700

    update 1

---
+++ app/src/main/java/hcmute/edu/vn/hoangnhutkhanhngan_19110038/FragmentStudentInfo.java
@@ -0,0 +1,21 @@
+package hcmute.edu.vn.hoangnhutkhanhngan_19110038;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class FragmentStudentInfo extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_student_info, container, false);
+
+        return view;
+
+    }
+}
